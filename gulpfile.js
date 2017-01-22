7bbf089e3c3eebf1eefdc1c22d2d4f72b76e7562
Mario Giancini <mario.giancini@gmail.com>
2017-01-22 11:58:08 -0500

    Added npm package and gulpfile.

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+
+gulp.task('default', function() {
+   browserSync.init({
+       server: "./"
+   });
+   browserSync.stream();
+});
+
+gulp.task('styles', function() {
+  gulp.src('scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(gulp.dest('/css'));
+});
